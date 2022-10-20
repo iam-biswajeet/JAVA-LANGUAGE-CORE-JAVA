5108af2305a4dbbd5a091230229e1d459ddee71f
iam-biswajeet <biswajitsahoo9955@gmail.com>
2022-10-20 21:55:53 +0530

    io03

---
+++ FileIO/src/org/durgasoft/test/Test08.java
@@ -0,0 +1,26 @@
+package org.durgasoft.test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test08 {
+	public static void main(String[] args) throws IOException {
+		FileReader reader=new FileReader("1.txt");
+		int i=reader.read();
+		while(i!=-1) {
+			System.out.print((char)i);
+			i=reader.read();
+		}
+		System.out.println();
+		System.out.println("******************************");
+		File f=new File("1.txt");
+		char[] ch=new char[(int)f.length()];
+		FileReader fr=new FileReader(f);
+		
+		fr.read(ch);
+		String s=new String(ch);
+		System.out.println(s);
+	}
+
+}
---
+++ FileIO/src/org/durgasoft/test/Test09.java
@@ -0,0 +1,18 @@
+package org.durgasoft.test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Test09 {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter writer=new BufferedWriter(new FileWriter("2.txt"));
+		writer.write(100);
+		writer.newLine();
+		writer.write(new char[] {'a','b','c','d'});
+		writer.newLine();
+		writer.write("biswajit");
+		writer.flush();
+		writer.close();
+	}
+}
