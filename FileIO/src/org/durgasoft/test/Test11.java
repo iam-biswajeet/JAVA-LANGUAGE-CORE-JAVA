37ad366449739d57aef07c4ce9af107dac37e518
iam-biswajeet <biswajitsahoo9955@gmail.com>
2022-10-21 12:00:38 +0530

    file103

---
+++ FileIO/src/org/durgasoft/test/Test11.java
@@ -0,0 +1,19 @@
+package org.durgasoft.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test11 {
+	public static void main(String[] args) throws IOException {
+		PrintWriter pw=new PrintWriter("3.txt");
+		pw.write(100);
+		pw.println(100);
+		pw.println(true);
+		pw.println(10.5);
+		pw.println('a');
+		pw.println("I love My india");
+		pw.flush();
+		pw.close();
+	}
+
+}
---
+++ FileIO/src/org/durgasoft/test/Test13.java
@@ -0,0 +1,34 @@
+package org.durgasoft.test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test13 {
+	public static void main(String[] args) throws IOException{
+		PrintWriter pw=new PrintWriter("7.txt");
+		BufferedReader br1=new BufferedReader(new FileReader("src/org/durgasoft/test/4.txt"));
+		BufferedReader br2=new BufferedReader(new FileReader("src/org/durgasoft/test/5.txt"));
+		
+		String line1=br1.readLine();
+		String line2=br2.readLine();
+		while(line1!=null || line2!=null) {
+			if(line1!=null) {
+			pw.println(line1);
+			line1=br1.readLine();
+			}
+			if(line2!=null) {
+				pw.println(line2);
+				line2=br2.readLine();
+				}
+		}
+		 
+		
+		pw.flush();
+		pw.close();
+		br1.close();
+		br2.close();
+	}
+
+}
